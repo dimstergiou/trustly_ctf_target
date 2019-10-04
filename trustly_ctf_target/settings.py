82ba0dc93850453fae9185041bd8b70f7259de57
Dimitrios Stergiou <dimitrios.stergiou@trustly.com>
2019-10-04 14:56:07 +0200

    Added whitenoise support

--- trustly_ctf_target/settings.py
+++ trustly_ctf_target/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -126,3 +127,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIR =  (
     os.path.join(BASE_DIR, 'static'),
 )
+STATICFILE_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
